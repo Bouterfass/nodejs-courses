ee3c67c4b6b374e1e19c1354059afde8135360c9
Youcef Bouterfass <cefyou28@gmail.com>
2022-01-01 15:00:06 +0100

    HTML pages

--- routes/shop.js
+++ routes/shop.js
@@ -1,9 +1,15 @@
 const express = require('express');
-
+const path = require('path');
 const router = express.Router();
 
+
+
 router.get('/', (req, res, next) => {
-    res.send('<h1>Welcome to my shop!</h1>')
+
+    //res.send('<h1>Welcome to my shop!</h1>')
+    //on veut maintenant envoyer un fichier     HTML
+    //path permet de gerer les path sur tout les OS.
+    res.sendFile(path.join(__dirname, '../', 'views', 'shop.html'))
 })
 
 
